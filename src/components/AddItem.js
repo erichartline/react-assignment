dde558eb7b17af69bf5def8b8b1844bcd88c5f3c
wildlifehexagon <crossthebreeze@gmail.com>
2017-10-06 14:33:42 -0500

    add boilerplate + revise travis settings

--- src/components/AddItem.js
+++ src/components/AddItem.js
@@ -1,7 +1,22 @@
 import React, { Component } from 'react';
 
 class AddTodo extends Component {
-
+  // state = { value: "something" } // no need for constructor
+  //
+  //     static defaultProps = {
+  //         someProp: "somevalue"
+  //     }
+  //
+  //     method = () => {
+  //         // It is a bound method, means `this` is preserved
+  //     }
+  //
+  //     methodWithParams = (params) => {
+  //         // Bound method with passed parameters
+  //     }
+  //
+  //     render() { // the main render method
+  //     }
 }
 
 export default AddTodo;
